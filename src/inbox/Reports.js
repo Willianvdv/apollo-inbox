9b5c997163097fd6c22dbbd42c5e5a1d81f9966b
Siebe Jan <siebejan@hackerone.com>
2018-11-14 19:43:16 +0100

    show legacy report titles in inbox

--- src/inbox/Reports.js
+++ src/inbox/Reports.js
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { Suspense, useContext, useMemo } from 'react';
+import usePromise from 'react-use-promise';
 import {
   Row, Col, ListGroup, Button,
 } from 'reactstrap';
+
 import gql from 'graphql-tag';
 
 import { ApolloClient } from 'apollo-client';
@@ -30,60 +32,69 @@ const restReportQuery = gql`
   }
 `;
 
-hackeroneRestClient
-  .query({ query: restReportQuery, variables: { path: `reports/${427502}` } })
-  .then((response) => {
-    console.log('HELLO!');
-    console.log(response.data.report);
-  });
+const Report = ({ dispatch, report }) => {
+  const [legacyReport, legacyReportError] = usePromise(useMemo(
+    () => (
+      hackeroneRestClient
+        .query({ query: restReportQuery, variables: { path: `reports/${report.databaseId}` } })
+        .then(response => response.data.report)
+    ),
+    [],
+  ));
+
+  return (
+    <span
+      key={report.id}
+      onClick={() => dispatch({ type: actions.CHANGE_REPORT, payload: report.id })}
+      className="p-0 list-group-item list-group-item-action"
+    >
+      <Row className="p-0 py-3 mx-0">
+        <Col>
+          <img
+            className="rounded-circle float-left mr-3 mt-2"
+            src={report.team.profilePicture}
+            style={{ height: '35px' }}
+            alt={report.team.name}
+          />
+
+          <div>
+            <span className="text-dark" href="#">
+              {legacyReport ? legacyReport.title : 'Loading...'}
+            </span>
+            <div>
+              <small className="text-muted">
+                {report.substate}
+                {' '}
+                {report.databaseId}
+                {' | '}
+                <span>by </span>
+                {report.reporter.username}
+                {' | '}
+                <span> to </span>
+                <a
+                  href="#"
+                  onClick={event => dispatch({ type: actions.CHANGE_TEAM, payload: report.team.id })
+                      }
+                >
+                  {report.team.name}
+                </a>
+                {' '}
+              </small>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </span>
+  );
+};
 
 const Reports = ({ reports: { edges: reports } }) => {
   const dispatch = useContext(InboxDispatch);
+
   return (
     <ListGroup flush className="mr-2 pl-2">
       {reports.map(({ node: report }) => (
-        <span
-          key={report.id}
-          onClick={() => dispatch({ type: actions.CHANGE_REPORT, payload: report.id })}
-          className="p-0 list-group-item list-group-item-action"
-        >
-          <Row className="p-0 py-3 mx-0">
-            <Col>
-              <img
-                className="rounded-circle float-left mr-3 mt-2"
-                src={report.team.profilePicture}
-                style={{ height: '35px' }}
-                alt={report.team.name}
-              />
-
-              <div>
-                <span className="text-dark" href="#">
-                  Fake Report Title
-                </span>
-                <div>
-                  <small className="text-muted">
-                    {report.substate}
-                    {' '}
-                    {report.databaseId}
-                    {' | '}
-                    <span>by </span>
-                    {report.reporter.username}
-                    {' | '}
-                    <span> to </span>
-                    <a
-                      href="#"
-                      onClick={event => dispatch({ type: actions.CHANGE_TEAM, payload: report.team.id })
-                      }
-                    >
-                      {report.team.name}
-                    </a>
-                    {' '}
-                  </small>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </span>
+        <Report key={report.id} dispatch={dispatch} report={report} />
       ))}
     </ListGroup>
   );
