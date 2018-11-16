56e0e71b8c7a84c635208dc6f5f6f9c4f94ad531
Willian van der Velde <mail@willian.io>
2018-11-16 23:22:05 +0100

    More refined markdown rendering

--- src/inbox/Report.js
+++ src/inbox/Report.js
@@ -20,14 +20,36 @@ import SyntaxHighlighter from 'react-syntax-highlighter';
 // import { dark, coy } from 'react-syntax-highlighter/dist/styles/prism';
 import { docco } from 'react-syntax-highlighter/dist/styles/hljs';
 
+import reactStringReplace from 'react-string-replace';
 import { InboxDispatch, actions } from '../Inbox';
 import useEnhancedReport from './legacyReport';
+
+const X = ({ children }) => {
+  console.log(children);
+};
 
 const Code = ({ language, value }) => (
   <SyntaxHighlighter className="p-3 ml-4 border-left" language={language} style={docco}>
     {value}
   </SyntaxHighlighter>
 );
+
+const Text = ({ value }) => {
+  // Abbreviations
+  let v = reactStringReplace(value, /\b(xss|rce|sqli|csp)\b/gi, match => (
+    <span className="border-bottom border-info">{match}</span>
+  ));
+
+  // Replace the FXXXX references by images
+  v = reactStringReplace(v, /\{F(\d+)\}/gi, match => (
+    <span className="badge badge-danger">
+IMAGE REF ERROR! I do not know the src of #
+      {match}
+    </span>
+  ));
+
+  return v;
+};
 
 const Heading = ({ level, children }) => React.createElement(`h${level}`, { className: `h${level + 2}` }, children);
 
@@ -76,6 +98,7 @@ const Report = ({ reportId }) => {
     ...report,
   };
 
+  console.log(report);
   return (
     <Fade>
       <Card>
@@ -124,7 +147,7 @@ const Report = ({ reportId }) => {
             <small className="text-muted">
               <span>Reported </span>
               {formatDistance(report.created_at, new Date())}
-              <span> to </span>
+              <span> ago to </span>
               <button onClick={event => dispatch({ type: actions.CHANGE_TEAM, payload: team.id })}>
                 {team.name}
               </button>
@@ -137,7 +160,7 @@ const Report = ({ reportId }) => {
         <CardBody>
           <div className="vulnerability-information-html">
             <ReactMarkdown
-              renderers={{ code: Code, heading: Heading }}
+              renderers={{ code: Code, heading: Heading, text: Text }}
               source={report.vulnerability_information}
               escapeHtml
             />
