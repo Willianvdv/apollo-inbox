8b31cdbb7f1f6641075b6208f34a287b92aa43ab
Willian van der Velde <mail@willian.io>
2018-11-17 23:27:51 +0100

    Remove unused subheader

--- src/Inbox.js
+++ src/Inbox.js
@@ -58,12 +58,6 @@ const Inbox = () => {
     <InboxDispatch.Provider value={dispatch}>
       <Container fluid>
         <Fade>
-          <Row className="bg-light py-2 border-bottom border-medium">
-            <span className="border-right pl-3 pr-2">Search:</span>
-            <span className="border-right px-3">TODO: TeamSelector</span>
-            <span className="border-right px-3">Hacker filter</span>
-            <span className="border-right px-3">Substate filter</span>
-          </Row>
           {state.teamId && (
             <Suspense
               fallback={(
