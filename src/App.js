5a1a4e3a8662899ed1dc1155d7bb6b554b3d70d0
Willian van der Velde <mail@willian.io>
2018-11-17 22:35:10 +0100

    Moving stuff around

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Suspense } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
-import ApolloClient from "apollo-boost";
-import Inbox from "./Inbox";
-import Loading from "./Loading";
+import React, { Suspense } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+import ApolloClient from 'apollo-boost';
+import Inbox from './Inbox';
+import Loading from './Loading';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: 'https://yahi-server.herokuapp.com/',
 });
 
 const App = () => (
