ce179212d5393ccb045e94f7196f20d40d53517e
Willian van der Velde <mail@willian.io>
2018-11-18 09:55:35 +0100

    oops

--- src/inbox/Report.js
+++ src/inbox/Report.js
@@ -1,8 +1,11 @@
-import { formatDistance } from 'date-fns';
-import { useApolloQuery } from 'react-apollo-hooks';
-import React, { useContext } from 'react';
-import gql from 'graphql-tag';
-import ReactMarkdown from 'react-markdown';
+import { docco } from "react-syntax-highlighter/dist/styles/hljs";
+import { formatDistance } from "date-fns";
+import { useApolloQuery } from "react-apollo-hooks";
+import React, { useContext } from "react";
+import ReactMarkdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import gql from "graphql-tag";
+import reactStringReplace from "react-string-replace";
 
 import {
   Col,
@@ -11,25 +14,18 @@ import {
   Row,
   CardBody,
   CardHeader,
-  CardText,
   ListGroup,
-  ListGroupItem,
-} from 'reactstrap';
+  ListGroupItem
+} from "reactstrap";
 
-import SyntaxHighlighter from 'react-syntax-highlighter';
-// import { dark, coy } from 'react-syntax-highlighter/dist/styles/prism';
-import { docco } from 'react-syntax-highlighter/dist/styles/hljs';
-
-import reactStringReplace from 'react-string-replace';
-import { InboxDispatch, actions } from '../Inbox';
-import useEnhancedReport from './legacyReport';
-
-const X = ({ children }) => {
-  console.log(children);
-};
+import { InboxDispatch, actions } from "../Inbox";
 
 const Code = ({ language, value }) => (
-  <SyntaxHighlighter className="p-3 ml-4 border-left" language={language} style={docco}>
+  <SyntaxHighlighter
+    className="p-3 ml-4 border-left"
+    language={language}
+    style={docco}
+  >
     {value}
   </SyntaxHighlighter>
 );
@@ -43,60 +39,56 @@ const Text = ({ value }) => {
   // Replace the FXXXX references by images
   v = reactStringReplace(v, /\{F(\d+)\}/gi, match => (
     <span className="badge badge-danger">
-IMAGE REF ERROR! I do not know the src of #
-      {match}
+      IMAGE REF ERROR! I do not know the src of #{match}
     </span>
   ));
 
   return v;
 };
 
-const Heading = ({ level, children }) => React.createElement(`h${level}`, { className: `h${level + 2}` }, children);
+const Heading = ({ level, children }) =>
+  React.createElement(`h${level}`, { className: `h${level + 2}` }, children);
 
 const Report = ({ reportId }) => {
   const dispatch = useContext(InboxDispatch);
 
   const { data } = useApolloQuery(
     gql`
-      query Report($reportId: ID!) {
-        report: node(id: $reportId) {
-          ... on Report {
-            databaseId: _id
-            substate
-            created_at
-            disclosed_at
+      query Report($reportId: Int!) {
+        report(id: $reportId) {
+          id
+          substate
+          title
+          vulnerabilityInformation: vulnerability_information
+          createdAt
+          disclosedAt
 
-            team {
-              id
-              name
-              handle
-              profilePicture: profile_picture(size: small)
-            }
+          team {
+            id
+            name
+            handle
+          }
 
-            reporter {
-              name
-              username
-              reputation
-              rank
-              signal
-              signal_percentile
-              impact
-              profilePicture: profile_picture(size: small)
-            }
+          reporter {
+            name
+            username
+            reputation
+            rank
+            signal
+            signalPercentile
+            impact
+            profilePicture
           }
         }
       }
     `,
-    { variables: { reportId } },
+    { variables: { reportId } }
   );
 
-  let { report } = data;
-  const { reporter, team } = report;
-
-  report = {
-    ...useEnhancedReport(report.databaseId),
-    ...report,
-  };
+  const {
+    report,
+    report: { reporter, team }
+  } = data;
 
   return (
     <Fade>
@@ -107,27 +99,19 @@ const Report = ({ reportId }) => {
             <img
               className="rounded-circle ml-4 mr-2 border border-secondary float-left"
               src={reporter.profilePicture}
-              style={{ height: '20px' }}
+              style={{ height: "20px" }}
               alt={reporter.username}
             />
           </Col>
           <Col md="6" className="text-right pt-2 pr-4">
             <span className="font-weight-light">
-              Reputation:
-              {' '}
-              {reporter.reputation}
-              {' | '}
-              Rank:
-              {' '}
-              {reporter.rank}
-              {' | '}
-              Signal:
-              {' '}
-              {reporter.signal && reporter.signal.toFixed(2)}
-              {' | '}
-              Impact:
-              {' '}
-              {reporter.impact && reporter.impact.toFixed(2)}
+              Reputation: {reporter.reputation}
+              {" | "}
+              Rank: {reporter.rank}
+              {" | "}
+              Signal: {reporter.signal && reporter.signal.toFixed(2)}
+              {" | "}
+              Impact: {reporter.impact && reporter.impact.toFixed(2)}
             </span>
           </Col>
         </Row>
@@ -139,19 +123,23 @@ const Report = ({ reportId }) => {
         <ListGroup flush>
           <ListGroupItem>
             <small className="text-muted">
-              {'#'}
+              {"#"}
               {report.databaseId}
-              {' - '}
+              {" - "}
               {report.substate}
-              {' - '}
+              {" - "}
               <span>Reported </span>
               {formatDistance(report.created_at, new Date())}
               <span> ago to </span>
-              <button onClick={event => dispatch({ type: actions.CHANGE_TEAM, payload: team.id })}>
+              <button
+                onClick={event =>
+                  dispatch({ type: actions.CHANGE_TEAM, payload: team.id })
+                }
+              >
                 {team.name}
               </button>
               <span> and disclosed </span>
-              {formatDistance(report.disclosed_at, new Date())}
+              {formatDistance(report.disclosedAt, new Date())}
               <span> ago</span>
             </small>
           </ListGroupItem>
@@ -160,7 +148,7 @@ const Report = ({ reportId }) => {
           <div className="vulnerability-information-html">
             <ReactMarkdown
               renderers={{ code: Code, heading: Heading, text: Text }}
-              source={report.vulnerability_information}
+              source={report.vulnerabilityInformation}
               escapeHtml
             />
           </div>
--- src/inbox/Reports.js
+++ src/inbox/Reports.js
@@ -1,66 +1,59 @@
-import React, { useContext } from 'react';
-import gql from 'graphql-tag';
+import React, { useContext } from "react";
+import gql from "graphql-tag";
 
-import {
-  Row, Col, ListGroup, ListGroupItem,
-} from 'reactstrap';
+import { Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
-import { InboxDispatch, actions } from '../Inbox';
-import useEnhancedReport from './legacyReport';
+import { InboxDispatch, actions } from "../Inbox";
 
-const Report = ({ dispatch, report: _report }) => {
-  const report = {
-    ...useEnhancedReport(_report.databaseId),
-    ..._report,
-  };
+const Report = ({ dispatch, report }) => (
+  <span
+    key={report.id}
+    onClick={() =>
+      dispatch({ type: actions.CHANGE_REPORT, payload: report.id })
+    }
+    className="p-0 list-group-item list-group-item-action"
+  >
+    <Row className="p-0 py-3 mx-0">
+      <Col>
+        <img
+          className="rounded-circle float-left mr-3 mt-2"
+          src={report.team.profilePicture}
+          style={{ height: "35px" }}
+          alt={report.team.name}
+        />
 
-  return (
-    <span
-      key={report.id}
-      onClick={() => dispatch({ type: actions.CHANGE_REPORT, payload: report.id })}
-      className="p-0 list-group-item list-group-item-action"
-    >
-      <Row className="p-0 py-3 mx-0">
-        <Col>
-          <img
-            className="rounded-circle float-left mr-3 mt-2"
-            src={report.team.profilePicture}
-            style={{ height: '35px' }}
-            alt={report.team.name}
-          />
-
+        <div>
+          <span className="text-dark" href="#">
+            {report.title}
+          </span>
           <div>
-            <span className="text-dark" href="#">
-              {report.title}
-            </span>
-            <div>
-              <small className="text-muted">
-                {report.substate}
-                {' '}
-                {report.databaseId}
-                {' | '}
-                <span>by </span>
-                {report.reporter.username}
-                {' | '}
-                <span> to </span>
-                <a
-                  href="#"
-                  onClick={event => dispatch({ type: actions.CHANGE_TEAM, payload: report.team.id })
-                  }
-                >
-                  {report.team.name}
-                </a>
-                {' '}
-              </small>
-            </div>
+            <small className="text-muted">
+              {report.substate} {report.databaseId}
+              {" | "}
+              <span>by </span>
+              {report.reporter.username}
+              {" | "}
+              <span> to </span>
+              <a
+                href="#"
+                onClick={event =>
+                  dispatch({
+                    type: actions.CHANGE_TEAM,
+                    payload: report.team.id
+                  })
+                }
+              >
+                {report.team.name}
+              </a>{" "}
+            </small>
           </div>
-        </Col>
-      </Row>
-    </span>
-  );
-};
+        </div>
+      </Col>
+    </Row>
+  </span>
+);
 
-const Reports = ({ reports: { edges: reports } }) => {
+const Reports = ({ reports }) => {
   const dispatch = useContext(InboxDispatch);
 
   return (
@@ -90,28 +83,24 @@ const Reports = ({ reports: { edges: reports } }) => {
 
 Reports.fragments = {
   reports: gql`
-    fragment InboxReports on ReportConnection {
-      edges {
-        node {
-          databaseId: _id
-          id
-          substate
-          disclosed_at
-          reporter {
-            name
-            username
-            reputation
-          }
-          team {
-            id
-            name
-            handle
-            profilePicture: profile_picture(size: small)
-          }
-        }
+    fragment InboxReports on Report {
+      id
+      title
+      substate
+      disclosedAt
+      reporter {
+        name
+        username
+        reputation
+      }
+      team {
+        id
+        name
+        handle
+        profilePicture
       }
     }
-  `,
+  `
 };
 
 export default Reports;
